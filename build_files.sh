a3fd88df0b1bac05047df6a14e4b789bb1aed421
Atul Kumar Singh <atul0003214@gmail.com>
2023-01-29 13:39:13 +0530

    1547

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 echo "install requirements"
 pip install -r requirements.txt
-
+python3.9 manage.py collectstatic --noinput --clear
 echo "start migrations"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-python3.9 manage.py collectstatic --noinput --clear
+
 
